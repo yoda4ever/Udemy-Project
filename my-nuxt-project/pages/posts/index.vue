1e836522764a2bc847d830892d1bb8383b3b3487
Leandro lima <leandrocrlima@gmail.com>
2019-04-18 18:35:13 -0300

    Primerio Commit do projeto

---
+++ my-nuxt-project/pages/posts/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="posts-page">
+
+
+    </div>
+</template>
+
+
+<style scoped>
+    .posts-page{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
