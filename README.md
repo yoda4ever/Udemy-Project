90a4b9fae8a5b61401d86a0880104727a0580ed5
Leandro lima <leandrocrlima@gmail.com>
2019-04-20 17:42:19 -0300

    Aula 27 completa

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 04-18-2019
 Last>> 26. Adding External Ponts.
 27.Creating the Landing Page. Next>>
+
+
+
+04-20-2019
+Last>> 27.Creating the Landing Page.
+28.Spliting the Page into Components. Next>>
+
--- my-nuxt-project/pages/index.vue
+++ my-nuxt-project/pages/index.vue
@@ -1,68 +1,107 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        my-nuxt-project
-      </h1>
-      <h2 class="subtitle">
-        My polished Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+    <div class="home-page">
+          <section class="intro">
+            <h1>Get the latest tech news!</h1> 
+          </section>
+          <section class="feature-posts">
+            <nuxt-link :to="'/posts/' + 1" class="post-preview">
+              <article >
+                <div class="post-thumbnail" style="background-image: url('https://cloud.oracle.com/opc/images/trends-hightech-4.jpg')"></div>
+                <div class="post-content">
+                  <h1>Post Title</h1>
+                  <p>Preview Text</p>
+                </div>
+              </article>
+            </nuxt-link>
+            <nuxt-link :to="'/posts/' + 2" class="post-preview">
+              <article >
+                <div class="post-thumbnail" style="background-image: url('http://singlepointstaffing.com/wp-content/uploads/2014/01/hand1.jpg')"></div>
+                <div class="post-content">
+                  <h1>Post Title 2</h1>
+                  <p>Preview Text</p>
+                </div>
+              </article>
+            </nuxt-link>
+          </section>
     </div>
-  </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<style scope>
+  .intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
 
-export default {
-  components: {
-    Logo
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
   }
 }
-</script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.featured-posts {
   display: flex;
-  justify-content: center;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
   align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
+
+
